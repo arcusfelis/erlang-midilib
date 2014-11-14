53bc914bf019b353bf0f60c1155218d02ca342a8
Michael Uvarov <arcusfelis@gmail.com>
2014-11-14 01:09:22 +0100

    Make MIDI encoding pure

--- src/midifile.erl
+++ src/midifile.erl
@@ -109,7 +109,7 @@ read_event(DeltaTime, _, _,
 read_event(DeltaTime, _, _,
 	       <<?STATUS_NIBBLE_PITCH_BEND:4, Chan:4, 0:1, LSB:7, 0:1, MSB:7, Bin/binary>>) ->
     Status = ?STATUS_NIBBLE_PITCH_BEND,
-    Event = {pitch_bend, DeltaTime, [Chan, <<0:2, MSB:7, LSB:7>>]},
+    Event = {pitch_bend, DeltaTime, [Chan, MSB, LSB]},
     {Event, Status, Chan, Bin};
 read_event(DeltaTime, _, _,
 	       <<?STATUS_META_EVENT:8, ?META_TRACK_END:8, 0:8, Bin/binary>>) ->
@@ -158,8 +158,8 @@ parse_meta_event(Type, DeltaTime, Data) ->
 	    {midi_chan_prefix, DeltaTime, [Data]};
 	?META_SET_TEMPO ->
 	    % Data is microseconds per quarter note, in three bytes
-	    <<B0:8, B1:8, B2:8>> = Data,
-	    {tempo, DeltaTime, [(B0 bsl 16) + (B1 bsl 8) + B2]};
+	    <<Value:24>> = Data,
+	    {tempo, DeltaTime, [Value]};
 	?META_SMPTE ->
 	    {smpte, DeltaTime, [Data]};
 	?META_TIME_SIG ->
@@ -183,140 +183,195 @@ read_var_len(<<1:1, B0:7, 1:1, B1:7, 1:1, B2:7, 0:1, B3:7, Bin/binary>>) ->
 read_var_len(<<1:1, B0:7, 1:1, B1:7, 1:1, B2:7, 1:1, B3:7, Bin/binary>>) ->
     {4, (B0 bsl 21) + (B1 bsl 14) + (B2 bsl 7) + B3, Bin}.
 
-write({seq, Header, ConductorTrack, Tracks}, Path) ->
-    L = [header_io_list(Header, length(Tracks) + 1) |
-	 lists:map(fun(T) -> track_io_list(T) end, [ConductorTrack | Tracks])],
-    ok = file:write_file(Path, L).
+write(Seq, Path) ->
+    Encoded = seq_to_iolist(Seq),
+    file:write_file(Path, Encoded).
 
-header_io_list(Header, NumTracks) ->
-    {header, _, Division} = Header,
-    ["MThd",
-     0, 0, 0, 6,				% header chunk size
-     0, 1,					% format,
-     (NumTracks bsr 8) band 255,		% num tracks
-      NumTracks        band 255,
-     (Division bsr 8) band 255,			% division
-      Division        band 255].
+seq_to_iolist({seq, Header, ConductorTrack, Tracks}) ->
+    AllTracks = [ConductorTrack | Tracks],
+    [header_to_binary(Header, length(AllTracks)) |
+	 lists:map(fun track_to_binary/1, AllTracks)].
 
-track_io_list(Track) ->
+header_to_binary({header, _, Division}, NumTracks) ->
+    BytesToRead = 6,
+    Format = 1,
+    make_header(BytesToRead, Format, NumTracks, Division).
+
+make_header(BytesToRead, Format, NumTracks, Division) ->
+    <<"MThd", BytesToRead:32, Format:16, NumTracks:16, Division:16>>.
+
+track_to_binary(Track) ->
     {track, Events} = Track,
     put(status, 0),
     put(chan, 0),
-    EventList =  lists:map(fun(E) -> event_io_list(E) end, Events),
-    ChunkSize = chunk_size(EventList),
-    ["MTrk",
-     (ChunkSize bsr 24) band 255,
-     (ChunkSize bsr 16) band 255,
-     (ChunkSize bsr  8) band 255,
-      ChunkSize         band 255,
-     EventList].
+    EventList =  encode_events(Events),
+    Chunk = erlang:iolist_to_binary(EventList),
+    ChunkSize = byte_size(Chunk),
+    <<"MTrk", ChunkSize:32, Chunk/binary>>.
 
-% Return byte size of L, which is an IO list that contains lists, bytes, and
-% binaries.
-chunk_size(L) ->
-    lists:foldl(fun(E, Acc) -> Acc + io_list_element_size(E) end, 0,
-		lists:flatten(L)).
-io_list_element_size(E) when is_binary(E) ->
-    size(E);
-io_list_element_size(_E) ->
-    1.
+encode_events(Events) ->
+    encode_events(Events, 0, 0, []).
 
-event_io_list({off, DeltaTime, [Chan, Note, Vel]}) ->
-    RunningStatus = get(status),
-    RunningChan = get(chan),
-    if
-	RunningChan =:= Chan,
-	    (RunningStatus =:= ?STATUS_NIBBLE_OFF orelse
-	     (RunningStatus =:= ?STATUS_NIBBLE_ON andalso Vel =:= 64)) ->
-	    Status = [],
-	    OutVel = 0;
-	true ->
-	    Status = (?STATUS_NIBBLE_OFF bsl 4) + Chan,
-	    OutVel = Vel,
-	    put(status, ?STATUS_NIBBLE_OFF),
-	    put(chan, Chan)
-    end,
-    [var_len(DeltaTime), Status, Note, OutVel];
-event_io_list({on, DeltaTime, [Chan, Note, Vel]}) ->
-    [var_len(DeltaTime), running_status(?STATUS_NIBBLE_ON, Chan), Note, Vel];
-event_io_list({poly_press, DeltaTime, [Chan, Note, Amount]}) ->
-    [var_len(DeltaTime), running_status(?STATUS_NIBBLE_POLY_PRESS, Chan), Note,
-     Amount];
-event_io_list({controller, DeltaTime, [Chan, Controller, Value]}) ->
-    [var_len(DeltaTime), running_status(?STATUS_NIBBLE_CONTROLLER, Chan),
-     Controller, Value];
-event_io_list({program, DeltaTime, [Chan, Program]}) ->
-    [var_len(DeltaTime), running_status(?STATUS_NIBBLE_PROGRAM_CHANGE, Chan),
-     Program];
-event_io_list({chan_press, DeltaTime, [Chan, Amount]}) ->
-    [var_len(DeltaTime), running_status(?STATUS_NIBBLE_CHANNEL_PRESSURE, Chan),
-     Amount];
-event_io_list({pitch_bend, DeltaTime, [Chan, <<0:2, MSB:7, LSB:7>>]}) ->
-    [var_len(DeltaTime), running_status(?STATUS_NIBBLE_PITCH_BEND, Chan),
-     <<0:1, LSB:7, 0:1, MSB:7>>];
-event_io_list({track_end, DeltaTime, []}) ->
-    ?DPRINT("track_end~n", []),
-    put(status, ?STATUS_META_EVENT),
-    [var_len(DeltaTime), ?STATUS_META_EVENT, ?META_TRACK_END, 0];
-event_io_list({seq_num, DeltaTime, [Data]}) ->
-    meta_io_list(DeltaTime, ?META_SEQ_NUM, Data);
-event_io_list({text, DeltaTime, Data}) ->
-    meta_io_list(DeltaTime, ?META_TEXT, Data);
-event_io_list({copyright, DeltaTime, Data}) ->
-    meta_io_list(DeltaTime, ?META_COPYRIGHT, Data);
-event_io_list({seq_name, DeltaTime, Data}) ->
-    put(status, ?STATUS_META_EVENT),
-    meta_io_list(DeltaTime, ?META_TRACK_END, Data);
-event_io_list({instrument, DeltaTime, Data}) ->
-    meta_io_list(DeltaTime, ?META_INSTRUMENT, Data);
-event_io_list({lyric, DeltaTime, Data}) ->
-    meta_io_list(DeltaTime, ?META_LYRIC, Data);
-event_io_list({marker, DeltaTime, Data}) ->
-    meta_io_list(DeltaTime, ?META_MARKER, Data);
-event_io_list({cue, DeltaTime, Data}) ->
-    meta_io_list(DeltaTime, ?META_CUE, Data);
-event_io_list({midi_chan_prefix, DeltaTime, [Data]}) ->
-    meta_io_list(DeltaTime, ?META_MIDI_CHAN_PREFIX, Data);
-event_io_list({tempo, DeltaTime, [Data]}) ->
-    ?DPRINT("tempo, data = ~p~n", [Data]),
-    put(status, ?STATUS_META_EVENT),
-    [var_len(DeltaTime), ?STATUS_META_EVENT, ?META_SET_TEMPO, var_len(3),
-     (Data bsr 16) band 255,
-     (Data bsr  8) band 255,
-      Data         band 255];
-event_io_list({smpte, DeltaTime, [Data]}) ->
-    meta_io_list(DeltaTime, ?META_SMPTE, Data);
-event_io_list({time_signature, DeltaTime, [Data]}) ->
-    meta_io_list(DeltaTime, ?META_TIME_SIG, Data);
-event_io_list({key_signature, DeltaTime, [Data]}) ->
-    meta_io_list(DeltaTime, ?META_KEY_SIG, Data);
-event_io_list({sequencer_specific, DeltaTime, [Data]}) ->
-    meta_io_list(DeltaTime, ?META_SEQUENCER_SPECIFIC, Data);
-event_io_list({unknown_meta, DeltaTime, [Type, Data]}) ->
-    meta_io_list(DeltaTime, Type, Data).
+encode_events([Event|Events], RunningChan, RunningStatus, Acc) ->
+    {Encoded, RunningChan1, RunningStatus1} =
+        encode_event(RunningChan, RunningStatus, Event),
+    encode_events(Events, RunningChan1, RunningStatus1, [Encoded|Acc]);
+encode_events([], _RunningChan, _RunningStatus, Acc) ->
+    lists:reverse(Acc).
 
-meta_io_list(DeltaTime, Type, Data) when is_binary(Data) ->
-    ?DPRINT("meta_io_list (bin) type = ~p, data = ~p~n", [Type, Data]),
-    put(status, ?STATUS_META_EVENT),
-    [var_len(DeltaTime), ?STATUS_META_EVENT, Type, var_len(size(Data)), Data];
-meta_io_list(DeltaTime, Type, Data) ->
-    ?DPRINT("meta_io_list type = ~p, data = ~p~n", [Type, Data]),
-    put(status, ?STATUS_META_EVENT),
-    [var_len(DeltaTime), ?STATUS_META_EVENT, Type, var_len(length(Data)), Data].
 
-running_status(HighNibble, Chan) ->
-    RunningStatus = get(status),
-    RunningChan = get(chan),
-    if
-	RunningStatus =:= HighNibble, RunningChan =:= Chan ->
-	    ?DPRINT("running status: stat = ~p, rchan = ~p~n",
-		    [RunningStatus, RunningChan]),
-	    [];
-	true ->
-	    put(status, HighNibble),
-	    put(chan, Chan),
-	    (HighNibble bsl 4) + Chan
-    end.
+encode_event(RunningChan, RunningStatus,
+             {off, DeltaTime, [Chan, Note, 64]}) ->
+    %% Off with velocity=64 is on
+    Status = ?STATUS_NIBBLE_ON,
+    D = encode_delta(DeltaTime),
+    S = encode_running_status(RunningStatus, RunningChan, Status, Chan),
+    Encoded = [D, S, Note, 0],
+    {Encoded, Chan, Status};
+encode_event(RunningChan, RunningStatus,
+             {off, DeltaTime, [Chan, Note, Vel]}) ->
+    Status = ?STATUS_NIBBLE_OFF,
+    D = encode_delta(DeltaTime),
+    S = encode_running_status(RunningStatus, RunningChan, Status, Chan),
+    Encoded = [D, S, Note, Vel],
+    {Encoded, Chan, Status};
+encode_event(RunningChan, RunningStatus,
+             {on, DeltaTime, [Chan, Note, Vel]}) ->
+    Status = ?STATUS_NIBBLE_ON,
+    D = encode_delta(DeltaTime),
+    S = encode_running_status(RunningStatus, RunningChan, Status, Chan),
+    Encoded = [D, S, Note, Vel],
+    {Encoded, Chan, Status};
+encode_event(RunningChan, RunningStatus,
+             {poly_press, DeltaTime, [Chan, Note, Amount]}) ->
+    Status = ?STATUS_NIBBLE_POLY_PRESS,
+    D = encode_delta(DeltaTime),
+    S = encode_running_status(RunningStatus, RunningChan, Status, Chan),
+    Encoded = [D, S, Note, Amount],
+    {Encoded, Chan, Status};
+encode_event(RunningChan, RunningStatus,
+             {controller, DeltaTime, [Chan, Controller, Value]}) ->
+    Status = ?STATUS_NIBBLE_CONTROLLER,
+    D = encode_delta(DeltaTime),
+    S = encode_running_status(RunningStatus, RunningChan, Status, Chan),
+    Encoded = [D, S, Controller, Value],
+    {Encoded, Chan, Status};
+encode_event(RunningChan, RunningStatus,
+             {program, DeltaTime, [Chan, Program]}) ->
+    Status = ?STATUS_NIBBLE_PROGRAM_CHANGE,
+    D = encode_delta(DeltaTime),
+    S = encode_running_status(RunningStatus, RunningChan, Status, Chan),
+    Encoded = [D, S, Program],
+    {Encoded, Chan, Status};
+encode_event(RunningChan, RunningStatus,
+             {chan_press, DeltaTime, [Chan, Amount]}) ->
+    Status = ?STATUS_NIBBLE_PROGRAM_CHANGE,
+    D = encode_delta(DeltaTime),
+    S = encode_running_status(RunningStatus, RunningChan, Status, Chan),
+    Encoded = [D, S, Amount],
+    {Encoded, Chan, Status};
+encode_event(RunningChan, RunningStatus,
+             {pitch_bend, DeltaTime, [Chan, Amount]}) ->
+    Status = ?STATUS_NIBBLE_PROGRAM_CHANGE,
+    D = encode_delta(DeltaTime),
+    S = encode_running_status(RunningStatus, RunningChan, Status, Chan),
+    Encoded = [D, S, Amount],
+    {Encoded, Chan, Status};
+encode_event(RunningChan, RunningStatus,
+             {pitch_bend, DeltaTime, [Chan, MSB, LSB]}) ->
+    %% TODO it probably can be simplier
+    Status = ?STATUS_NIBBLE_PITCH_BEND,
+    D = encode_delta(DeltaTime),
+    S = encode_running_status(RunningStatus, RunningChan, Status, Chan),
+    Encoded = [D, S, <<0:1, LSB:7, 0:1, MSB:7>>],
+    {Encoded, Chan, Status};
+encode_event(_RunningChan, _RunningStatus,
+             {track_end, DeltaTime, []}) ->
+    Status = ?STATUS_META_EVENT,
+    D = encode_delta(DeltaTime),
+    Encoded = [D, Status, ?META_TRACK_END, 0],
+    {Encoded, 0, Status};
+
+encode_event(RunningChan, RunningStatus,
+             {seq_num, DeltaTime, [Data]}) ->
+    Type = ?META_SEQ_NUM,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {text, DeltaTime, Data}) ->
+    Type = ?META_TEXT,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {copyright, DeltaTime, Data}) ->
+    Type = ?META_COPYRIGHT,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {seq_name, DeltaTime, Data}) ->
+    Type = ?META_TRACK_END,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {instrument, DeltaTime, Data}) ->
+    Type = ?META_INSTRUMENT,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {lyric, DeltaTime, Data}) ->
+    Type = ?META_LYRIC,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {marker, DeltaTime, Data}) ->
+    Type = ?META_MARKER,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {cue, DeltaTime, Data}) ->
+    Type = ?META_CUE,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {midi_chan_prefix, DeltaTime, [Data]}) ->
+    Type = ?META_MIDI_CHAN_PREFIX,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {tempo, DeltaTime, [Value]}) ->
+    Data = <<Value:24>>,
+    Type = ?META_SET_TEMPO,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {smpte, DeltaTime, [Data]}) ->
+    Type = ?META_SMPTE,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {time_signature, DeltaTime, [Data]}) ->
+    Type = ?META_TIME_SIG,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {key_signature, DeltaTime, [Data]}) ->
+    Type = ?META_KEY_SIG,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {sequencer_specific, DeltaTime, [Data]}) ->
+    Type = ?META_SEQUENCER_SPECIFIC,
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data);
+encode_event(RunningChan, RunningStatus,
+             {unknown_meta, DeltaTime, [Type, Data]}) ->
+    encode_meta_event(RunningStatus, RunningChan, DeltaTime, Type, Data).
+
+encode_meta_event(_RunningStatus, _RunningChan, DeltaTime, Type, Data) ->
+    Status = ?STATUS_META_EVENT,
+    Chan = 0,
+    D = encode_delta(DeltaTime),
+    L = var_len(iolist_size(Data)),
+    Encoded = [D, Status, Type, L, Data],
+    {Encoded, Chan, Status}.
+
+encode_running_status(Status, Chan, Status, Chan) ->
+    [];
+encode_running_status(_RunningStatus, _RunningChan, Status, Chan) ->
+    encode_status(Status, Chan).
+
+% <<Status:4, Chan:4>>
+encode_status(Status, Chan) ->
+    (Status bsl 4) + Chan.
+
+encode_delta(Delta) ->
+    var_len(Delta).
 
 var_len(I) when I < (1 bsl 7) ->
     <<0:1, I:7>>;
@@ -325,6 +380,4 @@ var_len(I) when I < (1 bsl 14) ->
 var_len(I) when I < (1 bsl 21) ->
     <<1:1, (I bsr 14):7, 1:1, (I bsr 7):7, 0:1, I:7>>;
 var_len(I) when I < (1 bsl 28) ->
-    <<1:1, (I bsr 21):7, 1:1, (I bsr 14):7, 1:1, (I bsr 7):7, 0:1, I:7>>;
-var_len(I) ->
-    exit("Value " ++ I ++ " is too big for a variable length number").
+    <<1:1, (I bsr 21):7, 1:1, (I bsr 14):7, 1:1, (I bsr 7):7, 0:1, I:7>>.
